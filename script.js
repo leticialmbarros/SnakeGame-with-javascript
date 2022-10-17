75c7472b8bf6e6f181a6a642eaa32c7510463a4c
Letícia Barros <106930665+leticialmbarros@users.noreply.github.com>
2022-10-17 20:52:49 -0300

    definindo direções e impedindo que a cobrinha suma

--- script.js
+++ script.js
@@ -10,18 +10,33 @@ snake[0] ={
 let direction = "right";
 
 function criarBG(){
-    context.fillStyle = "lightred";
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16*box, 16*box); 
 }
 
 function criarCobrinha (){
-    for(i=0; i < snake.length; i++){
-        context.fillStyle = "black";
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function iniciarJogo(){
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
         criarBG();
         criarCobrinha();
 
@@ -45,7 +60,9 @@ function iniciarJogo(){
         
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 200);
+
+
 
 
 
