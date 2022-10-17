129785f63c9ed75b8d730f18385496609c4dc50d
Letícia Barros <106930665+leticialmbarros@users.noreply.github.com>
2022-10-17 20:24:09 -0300

    definindo movimentos

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let canvas = document.getElementById("snake"); 
-let context = canvas.getContext("2d"); //....
+let context = canvas.getContext("2d"); 
 let box = 32;
 let snake = []; 
 snake[0] ={
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right";
-let food ={
-    x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
-}
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightred";
     context.fillRect(0, 0, 16*box, 16*box); 
 }
+
+function criarCobrinha (){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "red";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+criarBG();
+criarCobrinha();
