7ff090125cbefa7a1bff196c054b202576bda5f3
Letícia Barros <106930665+leticialmbarros@users.noreply.github.com>
2022-10-17 19:51:47 -0300

    criar background

---
+++ script.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("snake"); 
+let context = canvas.getContext("2d"); //....
+let box = 32;
+let snake = []; 
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box); 
+}
