139ebab617c6a0ac066187ad1672e62a7f762768
Letícia Barros <106930665+leticialmbarros@users.noreply.github.com>
2022-10-17 21:31:14 -0300

    criando a comidinha da cobrinha

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ snake[0] ={
     x: 8 * box,
     y: 8 * box
 }
-
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -19,6 +22,11 @@ function criarCobrinha (){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -39,6 +47,7 @@ function iniciarJogo(){
 
         criarBG();
         criarCobrinha();
+        drawFood ();
 
         let snakeX = snake[0].x;
         let snakeY = snake [0].y;
